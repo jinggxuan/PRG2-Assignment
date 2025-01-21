8991f4f7cd588f48fe49430b38c544aa2330518a
naisha <s10266823@connect.np.edu.sg>
2025-01-21 18:38:01 +0800

    Add files via upload

---
+++ prg2 final assgn/prg2 final assgn/Airlines.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg2_final_assgn
+{
+    public class Airline
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public Airline(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+    }
+    // yay
+}
