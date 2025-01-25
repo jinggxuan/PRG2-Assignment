591f5988e13924b3be0a1fa12b432a4f497199f8
jingxuan <s10267949@connect.np.edu.sg>
2025-01-25 21:15:28 +0800

    Add files via upload
    added LWTTFlight class

---
+++ LWTTFlight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg2_final_assgn
+{
+    public class LWTTFlight : Flight
+
+    {
+        public double RequestFee { get; set; } = 500;
+        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime)
+        : base(flightNumber, origin, destination, expectedTime) { }
+
+        public override double CalculateFees()
+        {
+            return base.CalculateFees() + RequestFee;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
