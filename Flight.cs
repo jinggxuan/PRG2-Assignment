ab07455d449cf69c4d28db548ff0aecb38f9888b
jingxuan <s10267949@connect.np.edu.sg>
2025-01-25 21:11:46 +0800

    Add files via upload
    added Flight Class

---
+++ Flight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg2_final_assgn
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; } = "On Time";
+
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+        }
+        public virtual double CalculateFees()
+        {
+            return Destination == "Singapore (SIN)" ? 500 : 800;
+        }
+
+        public override string ToString()
+        {
+            return $"Flight Number: {FlightNumber} \tOrigin: {Origin} \tDestination: {Destination} \tExpected Time: {ExpectedTime} \tStatus: {Status}";
+
+        }
+    }
+}
