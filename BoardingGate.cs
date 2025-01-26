5979eeb71bea2f8d05b2f7efd01168289922158c
naisha <s10266823@connect.np.edu.sg>
2025-01-26 16:19:46 +0800

    Update BoardingGate.cs

--- BoardingGate.cs
+++ BoardingGate.cs
@@ -8,35 +8,30 @@ namespace prg2_final_assgn
 {
     class BoardingGate
     {
-        private string gateName;
         public string GateName
         {
             get { return gateName; }
             set { gateName = value; }
         }
 
-        private bool supportsCFFT;
         public bool SupportsCFFT
         {
             get { return supportsCFFT; }
             set { supportsCFFT = value; }
         }
 
-        private bool supportsDDJB;
         public bool SupportsDDJB
         {
             get { return supportsDDJB; }
             set { supportsDDJB = value; }
         }
 
-        private bool supportsLWTT;
         public bool SupportsLWTT
         {
             get { return supportsLWTT; }
             set { supportsLWTT = value; }
         }
 
-        private Flight flight;
         public Flight Flight
         {
             get { return flight; }
