23ff9f83b0e75492d02eea8336ba759f031147fd
naisha <s10266823@connect.np.edu.sg>
2025-01-26 16:06:08 +0800

    Update BoardingGate.cs

--- BoardingGate.cs
+++ BoardingGate.cs
@@ -6,28 +6,76 @@ using System.Threading.Tasks;
 
 namespace prg2_final_assgn
 {
-    public class BoardingGate
+    class BoardingGate
     {
-        public string GateName { get; set; }
-        public bool SupportsCFFT { get; set; }
-        public bool SupportsDDJB { get; set; }
-        public bool SupportsLWTT { get; set; }
-        public Flight Flight { get; set; }
+        private string gateName;
+        public string GateName
+        {
+            get { return gateName; }
+            set { gateName = value; }
+        }
 
-        public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT)
+        private bool supportsCFFT;
+        public bool SupportsCFFT
+        {
+            get { return supportsCFFT; }
+            set { supportsCFFT = value; }
+        }
+
+        private bool supportsDDJB;
+        public bool SupportsDDJB
+        {
+            get { return supportsDDJB; }
+            set { supportsDDJB = value; }
+        }
+
+        private bool supportsLWTT;
+        public bool SupportsLWTT
+        {
+            get { return supportsLWTT; }
+            set { supportsLWTT = value; }
+        }
+
+        private Flight flight;
+        public Flight Flight
+        {
+            get { return flight; }
+            set { flight = value; }
+        }
+
+        public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT, Flight flight)
         {
             GateName = gateName;
             SupportsCFFT = supportsCFFT;
             SupportsDDJB = supportsDDJB;
             SupportsLWTT = supportsLWTT;
-            Flight = null; 
+            Flight = flight;
+        }
+
+        public double CalculateFees()
+        {
+            double baseFee = 300;
+            double additionalFee = 0;
+
+            if (SupportsCFFT == true)
+            {
+                additionalFee += 150;
+            }
+            if (SupportsDDJB == true)
+            {
+                additionalFee += 300;
+            }
+            if (SupportsLWTT == true)
+            {
+                additionalFee += 50;
+            }
+
+            return baseFee + additionalFee;
         }
 
         public override string ToString()
         {
-            var status = Flight == null ? "No flight assigned" : $"Assigned to flight {Flight.FlightNumber}";
-            return $"Gate {GateName} - Supports: " +
-                   $"CFFT({SupportsCFFT}), DDJB({SupportsDDJB}), LWTT({SupportsLWTT}) - {status}";
+            return $"Gate Name: {GateName} \t Supports CFFT: {SupportsCFFT} \t Supports DDJB: {SupportsDDJB} \t Supports LWTT: {SupportsLWTT}";
         }
     }
 }
