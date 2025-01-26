dd9e64cb90d720d294fc8b466553c200eef778fb
jingxuan <s10267949@connect.np.edu.sg>
2025-01-26 16:56:40 +0800

    Update BoardingGate.cs

--- BoardingGate.cs
+++ BoardingGate.cs
@@ -8,35 +8,12 @@ namespace prg2_final_assgn
 {
     class BoardingGate
     {
-        public string GateName
-        {
-            get { return gateName; }
-            set { gateName = value; }
-        }
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
 
-        public bool SupportsCFFT
-        {
-            get { return supportsCFFT; }
-            set { supportsCFFT = value; }
-        }
-
-        public bool SupportsDDJB
-        {
-            get { return supportsDDJB; }
-            set { supportsDDJB = value; }
-        }
-
-        public bool SupportsLWTT
-        {
-            get { return supportsLWTT; }
-            set { supportsLWTT = value; }
-        }
-
-        public Flight Flight
-        {
-            get { return flight; }
-            set { flight = value; }
-        }
+        public bool SupportsLWTT { get; set; }
+        public Flight flight { get; set; }
 
         public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT, Flight flight)
         {
@@ -44,28 +21,12 @@ namespace prg2_final_assgn
             SupportsCFFT = supportsCFFT;
             SupportsDDJB = supportsDDJB;
             SupportsLWTT = supportsLWTT;
-            Flight = flight;
+            this.flight = flight;
         }
 
         public double CalculateFees()
         {
-            double baseFee = 300;
-            double additionalFee = 0;
-
-            if (SupportsCFFT == true)
-            {
-                additionalFee += 150;
-            }
-            if (SupportsDDJB == true)
-            {
-                additionalFee += 300;
-            }
-            if (SupportsLWTT == true)
-            {
-                additionalFee += 50;
-            }
-
-            return baseFee + additionalFee;
+            return 300;
         }
 
         public override string ToString()
@@ -73,4 +34,5 @@ namespace prg2_final_assgn
             return $"Gate Name: {GateName} \t Supports CFFT: {SupportsCFFT} \t Supports DDJB: {SupportsDDJB} \t Supports LWTT: {SupportsLWTT}";
         }
     }
+}
 }
