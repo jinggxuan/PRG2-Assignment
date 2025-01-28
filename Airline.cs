cdcd6a271d8b045120dbda716a1d191980f597d8
jingxuan <s10267949@connect.np.edu.sg>
2025-01-28 22:36:19 +0800

    Update Airline.cs

--- Airline.cs
+++ Airline.cs
@@ -4,7 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prg2_final_assgn
+//==========================================================
+// Student Number	: S10266823
+// Student Name	: Maniar Naisha Keyur
+// Partner Name	: Wong Jing Xuan
+//==========================================================
+
+namespace S10266823_PRG2Assignment
 {
     class Airline
     {
